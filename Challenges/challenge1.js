5cb633099bfe5fd1b4482dddaebb9aee44e6ee92
Suraj-Razor <14635@coderacademy.edu.au>
2024-04-06 10:36:09 +1100

    feat! put in functions to stop code running automatically; added other functions

--- Challenges/challenge1.js
+++ Challenges/challenge1.js
@@ -1,81 +1,49 @@
-// 796. Rotate String
-// Easy
-
-// Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
-
-// A shift on s consists of moving the leftmost character of s to the rightmost position.
-
-// For example, if s = "abcde", then it will be "bcdea" after one shift.
- 
-
-// Example 1:
-
-// Input: s = "abcde", goal = "cdeab"
-// Output: true
-// Example 2:
-
-// Input: s = "abcde", goal = "abced"
-// Output: false
- 
-
-// Constraints:
-
-// 1 <= s.length, goal.length <= 100
-// s and goal consist of lowercase English letters.
-
-// let s = "abcde"
-// let goal = "cdeab"
-
-
-
-// var rotateString = function(s, goal) {
-//     if (s === goal) return true //check to see if the "s" is already equal to the "goal" improves leet code runtime
-//     let arr = s.split("") //convert chars in a string to an array
-//     for (let i = 0; i < arr.length; i++){
-//         let shiftedChar = arr.shift() //shift method removes first element on the array and returns the char which was removed
-//         arr.push(shiftedChar) //push method appends the removed char to the end of the array
-//         if (arr.join("") === goal) return true //join method converts the elements in an array to a string 
+// function fizzBuzz(start, end) {
+//     while(start<=end){
+//         if (start % 3 === 0 && start % 5 === 0) {
+//             console.log("fizzbuzz")
+//             return true
+//             start++
+//         } else if ( start % 3 === 0){
+//             console.log("fizz")
+//             start++
+//         } else if (start % 5 === 0){
+//             console.log("buzz")
+//             start++
+//         } else{
+//             console.log(start)
+//             start++
+//         }
+//         }
+//         return false
 //     }
-//     return false
-// };
+
+// fizzBuzz(1,25)
 
 
-// var rotateString = function(s, goal) {
-//     if (s.length !== goal.length) return false;
-//     console.log(s.concat(s))
-//     return s.concat(s).includes(goal);
-// };
-
-
-// var rotateString = function(s, goal) {
-//     // s = s.split("")
-//     for (let i = 0; i < s.length; i++){
-//         console.log(s.slice(1))
-//         // s = s.slice(1) + s[0];
-//         // console.log(s)
-//         if(s === goal) return true;
+// breakWhenPrivate = (arrayAll) => {
+//     for (arr of arrayAll){
+//         if (arr == "PRIVATE"){
+//             break;
+//         } else{
+//             console.log(arr);
+//         }
 //     }
-//     return false
-// };
-
-// console.log(rotateString(s,goal))
+// }
 
 
-directions = ["n", "s", "e", "e"]
+// breakWhenPrivate(["cat", "dog", "PRIVATE", "fish", "elephant"])
 
-// const setDirection = new Set(directions) 
-function loopOverArray(directions) {
-    let data  = {}
-    for (let direction of directions){
-        let count = 1
-        for (i=0;i<directions.length;i++){
-            if (directions[i] === direction) count++;
-        }
-        data.direction = count
-    }
-    // console.log(count)
-    return data
+// function capitaliseFirst(value) {
+//     value.map(value => value[]  )
+//     // your code here
+//   }
+
+let arr = ["here is one"]
+
+
+function capitalise(value) {
+    return value[0] + value.slice(1);
   }
 
-console.log(loopOverArray(directions))
-// console.log(setDirection)
+  console.log(capitalise(arr))
--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-let exampleEmptyVar = null
-let exampleEmptyVar1 = undefined
+function exampleStuff(){   
+    let exampleEmptyVar = null
+    let exampleEmptyVar1 = undefined
+    
+    console.log("Hello from script")
+    let message = "Hello from script"
+    
+    console.log(exampleEmptyVar, exampleEmptyVar1)
+    
+    console.log(`Hello ${message} `);
+    
+    world = "world"
+    
+    console.log("Hello" + world + "I'm Suraj")
+    
+    let fruit = [1,2,3,4,5]
+    
+    console.log("hello" + fruit)
+    
+    const someNewArray = new Array(10)
+    
+    console.log(someNewArray[1])
+}
 
-console.log("Hello from script")
-let message = "Hello from script"
+let pokemonTeam = [
+    "pikachu",
+    "squirtle",
+    "charmander",
+    "mew"
+]
 
-console.log(exampleEmptyVar, exampleEmptyVar1)
+function logTheTeam() {
+    sortedTeam = pokemonTeam.sort()
 
-console.log(`Hello ${message} `);
+    // sortedTeam.forEach((pokemon, arrayIndex) => {
+    //     console.log(pokemon + " is at index of " + arrayIndex)
+    // });
+    console.log(pokemonTeam.entries())
+    for (const [index, pokemon] of sortedTeam.enteries){
+        console.log()
+    }
+}
 
-world = "world"
+logTheTeam();
 
-console.log("Hello" + world + "I'm Suraj")
-
-let fruit = [1,2,3,4,5]
-
-console.log("hello" + fruit)
-
-const someNewArray = new Array(10)
-
-console.log(someNewArray[1])
+const logTheTeamAlternate = () => {
+    if (pokemonTeam.length == "2")
+}
